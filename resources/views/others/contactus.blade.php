6cf70b29198c180c8f17817853ad13f7208a9c80
staticpants123 <yanyansaberon@yahoo.com>
2017-02-23 02:24:34 +0800

    sample others
    about us, contact us, and faq

--- resources/views/others/contactus.blade.php
+++ resources/views/others/contactus.blade.php
@@ -7,5 +7,19 @@
 </head>
 <body>
 @include('partials.navbar')
+
+<div class="container">
+		<div class="row-bod">
+			<div class="col-md-12">
+				<div class="page-header">
+					<h4>How can we help you?</h4>
+				</div>
+
+				<h5>Address: Nasipit, Talamban Cebu City 6000</h5>
+				<h5>Email: emedservices@gmail.com</h5>
+			</div>
+		</div>
+	</div>
+
 </body>
 </html>
--- resources/views/others/faq.blade.php
+++ resources/views/others/faq.blade.php
@@ -7,5 +7,20 @@
 </head>
 <body>
 @include('partials.navbar')
+
+<div class="container">
+		<div class="row-bod">
+			<div class="col-md-12">
+				<div class="page-header">
+					<h4>Frequently Asked Questions</h4>
+				</div>
+
+				<h5><strong>What is your system about?</strong></h5>
+				<h6>- It is an online medical records management system.</h5>
+			</div>
+		</div>
+	</div>
+
+
 </body>
 </html>
