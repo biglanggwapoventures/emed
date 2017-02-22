dddd173258a232b1a99b626ad138a0b95040dfa4
wennaloo <wlrespina@gmail.com>
2017-02-22 20:09:57 +0800

    Updated positioning of forms

--- resources/views/managers/manager-form.blade.php
+++ resources/views/managers/manager-form.blade.php
@@ -4,7 +4,7 @@
 
  <div class="container-fluid">
     	<div class="row-bod">
-    		<div class="col-md-9">
+    		<div class="col-md-9 col-md-offset-1">
     			<div class="panel panel-default">
 			    	<div class="panel-heading">
 			    		<h4 class="panel-title"><i class="glyphicon glyphicon-user"></i>Register</h4>
