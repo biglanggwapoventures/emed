b947e6bd380e3e1a61fd28016d4e9a212f6abab4
wennaloo <wlrespina@gmail.com>
2017-02-23 00:43:20 +0800

    Aboutus

--- resources/views/others/aboutus.blade.php
+++ resources/views/others/aboutus.blade.php
@@ -1,4 +1,11 @@
-@extends('welcome')
-
-@section('body')
-@endsection
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/body.css') }}">
+</head>
+<body>
+@include('partials.navbar')
+</body>
+</html>
