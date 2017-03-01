a6b45333603c7c764673865c3da7b646ebcf405d
piepows <lpgakut@gmail.com>
2017-03-01 19:18:49 +0800

    Doctor's page

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -57,7 +57,8 @@ class UsersController extends Controller
      */
     public function edit($id)
     {
-        //
+        User::edit($id);
+        return redirect()->back();
     }
 
     /**
--- resources/views/doctors/doctor-home.blade.php
+++ resources/views/doctors/doctor-home.blade.php
@@ -1,28 +1,17 @@
 @extends('welcome')
 
 @section('body')
-<div class="container-fluid">
-	<div class="row-bod">
-		
-<div class="col-md-9 col-md-offset-1">
-    			<div class="panel panel-default"> 
-			    	<div class="panel-heading">
-			    		<h4 class="panel-title"><i class="glyphicon glyphicon-user"></i> Welcome Dr. {{ Auth::user()->fullname() }} </h4>
-			    	</div>
-					
-						
-  
-		
-							@foreach($items as $item)
-    
-     Clinic: {{$item->clinic}}
-      
 
-   
-@endforeach
-		
-					</div>
-					</div>
-					</div>
+<div class="container-fluid">
+	<div class="row-bod">		
+		<div class="col-md-9 col-md-offset-1">
+    		<div class="panel panel-default"> 
+			   	<div class="panel-heading">
+		    		<h4 class="panel-title"><i class="glyphicon glyphicon-user"></i> Welcome Dr. {{ Auth::user()->fullname() }} </h4>
+			    </div>
+			</div>
+		</div>
+	</div>
+</div>
 
 @endsection
