a84e0d682502b5742733060c72f07b8594cd4ae9
staticpants123 <yanyansaberon@yahoo.com>
2017-03-01 12:54:59 +0800

    slut

--- resources/views/others/aboutus.blade.php
+++ resources/views/others/aboutus.blade.php
@@ -18,6 +18,7 @@
 				
 				<p>
 					E-med services is a startup company that provides quality medical services to medical establishments and pharmacies. It was founded by a group of college students who  were doing it as a thesis and have thought that it could aid people's needs. The company's system aims to solve the problems of the paper-based system.
+					Driane is a slut foreverrrrrr
 				</p>
 			</div>
 		</div>
