d19bd57051bc215309d4e4f03757d7cb940d2775
jcaroles <johncarolehernandez8@gmail.com>
2017-03-17 05:44:25 -0700

    Secretary Edit

--- resources/views/secretary/edit.blade.php
+++ resources/views/secretary/edit.blade.php
@@ -18,6 +18,7 @@
                             <div class="col-md-4">
                                 <div class="form-group {{ $errors->has('firstname') ? 'has-error' : '' }}">
                                     <label class="control-label">First Name</label>
+                                    {!! Form::text('firstname', $data->userInfo->firstname, ['class' => 'form-control']) !!} @if($errors->has('firstname'))
                                     <span class="help-block">{{ $errors->first('firstname') }}</span> @endif
                                 </div>
                             </div>
@@ -26,6 +27,7 @@
                                 <div class="form-group {{ $errors->has('middle_initial') ? 'has-error' : '' }}">
                                     <label class="control-label">Middle Initial</label>
                                     <span style="color: red">*</span>
+                                    {!! Form::text('middle_initial', $data->userInfo->middle_initial, ['class' => 'form-control']) !!} @if($errors->has('middle_initial'))
                                     <span class="help-block">{{ $errors->first('middle_initial') }}</span> @endif
                                 </div>
                             </div>
@@ -33,6 +35,7 @@
                                 <div class="form-group {{ $errors->has('lastname') ? 'has-error' : '' }}">
                                     <label class="control-label">Last Name</label>
                                     <span style="color: red">*</span>
+                                    {!! Form::text('lastname', $data->userInfo->lastname, ['class' => 'form-control']) !!} @if($errors->has('lastname'))
                                     <span class="help-block">{{ $errors->first('lastname') }}</span> @endif
                                 </div>
                             </div>
@@ -54,11 +57,13 @@
                             <div class="col-md-4">
                                 <div class="form-group">
                                     <label class="control-label">Birthdate <span style="color: red">*</span></label>
+                                    {!! Form::date('birthdate', $data->userInfo->birthdate, ['class' => 'form-control'], ['maxlength' => 100]) !!}
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group {{ $errors->has('contact_number') ? 'has-error' : '' }}">
                                     <label class="control-label">Contact No.</label>
+                                    {!! Form::text('contact_number', $data->userInfo->contact_number, ['class' => 'form-control']) !!} @if($errors->has('contact_number'))
                                     <span class="help-block">{{ $errors->first('contact_number') }}</span> @endif
                                 </div>
                             </div>
@@ -67,6 +72,7 @@
                                 <div class="form-group {{ $errors->has('address') ? 'has-error' : '' }}">
                                     <label class="control-label">Home Address</label>
                                     <span style="color: red">*</span>
+                                    {!! Form::text('address', $data->userInfo->address, ['class' => 'form-control']) !!} @if($errors->has('address'))
                                     <span class="help-block">{{ $errors->first('address') }}</span> @endif
                                 </div>
                             </div>
@@ -77,12 +83,14 @@
                             <div class="col-md-4">
                                 <div class="form-group {{ $errors->has('username') ? 'has-error' : '' }}">
                                     <label class="control-label">Username</label>
+                                    {!! Form::text('username', $data->userInfo->username, ['class' => 'form-control']) !!} @if($errors->has('username'))
                                     <span class="help-block">{{ $errors->first('username') }}</span> @endif
                                 </div>
                             </div>
                             <div class="col-md-4">
                                 <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
                                     <label class="control-label">Email</label>
+                                    {!! Form::text('email', $data->userInfo->email, ['class' => 'form-control']) !!} @if($errors->has('email'))
                                     <span class="help-block">{{ $errors->first('email') }}</span> @endif
                                 </div>
                             </div>
@@ -100,15 +108,18 @@
                             <div class="col-md-4">
                                 <div class="form-group {{ $errors->has('attainment') ? 'has-error' : '' }}">
                                     <label class="control-label">Attainment</label>
+                                    {!! Form::text('attainment', $data->attainment, ['class' => 'form-control']) !!} @if($errors->has('attainment'))
                                     <span class="help-block">{{ $errors->first('attainment') }}</span> @endif
                                 </div>
                             </div>
 
+                           <!--  <div class="col-md-4">
                                 <div class="form-group {{ $errors->has('license') ? 'has-error' : '' }}">
                                     <label class="control-label">License</label>
                                     <input type="text" name="license" class="form-control"> @if($errors->has('license'))
                                     <span class="help-block">{{ $errors->first('license') }}</span> @endif
                                 </div>
+                            </div> -->
                         </div>
 
 
