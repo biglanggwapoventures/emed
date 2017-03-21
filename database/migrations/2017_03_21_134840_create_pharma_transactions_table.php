c76974e24d8e7f409dee66a7eb4b2e87c990bc65
Nikki Gallego <mdag_03@yahoo.com>
2017-03-21 22:40:21 +0800

    transactions

---
+++ database/migrations/2017_03_21_134840_create_pharma_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePharmaTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pharma_transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('brand');
+            $table->string('genericname');
+            $table->integer('quantity');
+            $table->string('dosage');
+            $table->unsignedInteger('patient_id');
+            $table->unsignedInteger('pharma_id');
+            $table->foreign('patient_id')->references('id')->on('patients');
+            $table->foreign('pharma_id')->references('id')->on('pharmacists');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pharma_transactions');
+    }
+}
