08847c723c2fee4b21c4cf7623250d127c351f13
piepows <lpgakut@gmail.com>
2017-04-19 18:55:02 +0800

    Doctor Edit: Upload File Null Catch Error

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -14,11 +14,29 @@ class FileUploadController extends Controller
            'avatar' => 'image|max:2048',
         ]);
 
-        if ($validator->fails()) {
-            return redirect()->back()
+        // if ($validator->fails()) {
+        //     return redirect()->back()
+        //                 ->withErrors($validator)
+        //                 ->withInput();
+        // }
+
+      if ($request->hasFile('avatar')) {
+        $path = $request->file('avatar')->store(
+        'avatars/'.$request->id, 'public'
+        );
+
+        $user = Auth::user()->find($request->id);
+
+        $user->avatar = $path;
+        $user->save();
+
+        return redirect()->back();
+      }
+      else{
+         return redirect()->back()
                         ->withErrors($validator)
                         ->withInput();
-        }
+      }
 
 
 
@@ -29,15 +47,7 @@ class FileUploadController extends Controller
 
 
 
-		$path = $request->file('avatar')->store(
-		    'avatars/'.$request->id, 'public'
-		);
 
-		$user = Auth::user()->find($request->id);
-
-		$user->avatar = $path;
-		$user->save();
-
-		return redirect()->back();
+	
     }
 }
