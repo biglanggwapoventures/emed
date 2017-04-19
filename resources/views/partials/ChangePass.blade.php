d867b395ba364945349a2a2d451deb3ed46377c5
Nikki Gallego <mdag_03@yahoo.com>
2017-04-19 21:24:16 +0800

    mag media query nalang ko

--- resources/views/partials/ChangePass.blade.php
+++ resources/views/partials/ChangePass.blade.php
@@ -2,78 +2,86 @@
 <div class="content-wrapper">
     <div class="container-fluid">
 
-       
-      <div class="container">
-    <div class="row">
-        <div class="col-md-4 col-md-offset-3">
-            <div class="panel panel-default">
-                <div class="panel-heading"> <strong class="">Change Password</strong>
-
-                </div>
-                
-                <div class="panel-body">
-                    {!! Form::open(['url' => route('ChangePass'), 'method' => 'POST', 'enctype' => 'multipart/form-data'] , ['class' => 'form-control'] ,  ['role' => 'form']) !!}
-
+        <div class="text-center changepage" style="color: #346677;">
+            <div class="login-logo">Change Password</div>
+            <!-- Main Form -->
+            <div class="login-form-1">
+                {!! Form::open(['url' => route('ChangePass'), 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+                <div class="login-form-main-message"></div>
+                <div class="main-login-form">
+                    <div class="login-group">
                         <div class="form-group {{ $errors->has('current_password') ? 'has-error' : '' }}">
-                            <label for="inputPassword3" class="col-sm-3 control-label">Current</label>
-                            <div class="col-sm-9">
-                                <input type="password" name="current_password" class="form-control" id="inputPassword3" placeholder="Input Current Password" required="">
-                                 @if($errors->has('current_password'))
+                            <label class="control-label">Current Password</label>
+                            <input type="password" name="current_password" class="form-control">
+                             @if($errors->has('current_password'))
                             <span class="help-block">{{ $errors->first('current_password') }}</span> @endif
-                            </div>
                         </div>
                         <div class="form-group {{ $errors->has('new_password') || isset($new_password) ? 'has-error' : '' }}">
-                            <label for="inputPassword3" class="col-sm-3 control-label">New</label>
-                            <div class="col-sm-9">
-                                <input type="password" name="new_password" class="form-control" id="inputPassword3" placeholder="New Password" required="">
-                                  @if($errors->has('new_password'))
+
+                            <label class="control-label">New Password</label>
+                            <input type="password" name="new_password" class="form-control"> @if($errors->has('new_password'))
                             <span class="help-block">{{ $errors->first('new_password') }}</span> @endif @if(isset($new_password))
                             <span class="help-block">{{ $new_password }}</span> @endif
-                            <p>Must contain numbers uppercase lowercase symbols and number</p>
-                            </div>
+                            <p>Must be Alphanumeric</p>
                         </div>
-                         <div class="form-group {{ $errors->has('new_password_confirmation') || isset($new_password_confirmation) ? 'has-error' : '' }}">
-                            <label for="inputPassword3" class="col-sm-3 control-label">Confirm</label>
-                            <div class="col-sm-9">
-                                <input type="password" name="new_password_confirmation" class="form-control" id="inputPassword3" placeholder="Confirm New Password" required="">
-                                @if($errors->has('password'))
+                        <div class="form-group {{ $errors->has('new_password_confirmation') || isset($new_password_confirmation) ? 'has-error' : '' }}">
+                            <label class="control-label">Confirm New Password</label>
+                            <input type="password" name="new_password_confirmation" class="form-control"> @if($errors->has('password'))
                             <span class="help-block">{{ $errors->first('new_password_confirmation') }}</span> @endif @if(isset($new_password_confirmation))
                             <span class="help-block">{{ $new_password_confirmation }}</span> @endif
-                            </div>
                         </div>
-
-                        <div class="form-group last">
-                            <div class="col-sm-offset-3 col-sm-9">
-                                <button type="submit" class="btn btn-success btn-sm">Submit</button>
-                                
-                            </div>
-                        </div>
-                       {!! Form::close() !!}
+                    </div>
+                    <button type="submit" class="login-button"><i class="glyphicon glyphicon-chevron-right"></i></button>
                 </div>
-                 
-                <div class="panel-footer">Cancel? <a href="{{ url('/doctor-home') }}" class="">Back to profile</a>
-
-                </div>
-           
+                {!! Form::close() !!}
             </div>
+            <!-- end:Main Form -->
         </div>
+        <!-- <div class="row">
+    		<div class="col-md-4 col-md-offset-4">
+    			<div class="panel panel-default">
+			    	<div class="panel-heading">
+			    		<h4 class="panel-title"><i class="glyphicon glyphicon-lock"></i> Login</h4>
+			    	</div>
+			    	<div class="panel-body">
+			    		<form action="{{ url('/login') }}" method="POST">
+			    			{{ csrf_field() }}
+			    			<div class="form-group {{ $errors->has('username') ? 'has-error' : '' }}">
+			    				<label class="control-label">Enter your username</label>
+			    				<input type="text" name="username" class="form-control">
+			    				@if($errors->has('username'))
+			    					<span class="help-block">{{ $errors->first('username') }}</span>
+			    				@endif
+			    			</div>
+			    			<div class="form-group {{ $errors->has('password') || isset($wrongPassword) ? 'has-error' : '' }}">
+			    				<label class="control-label">Enter your password</label>
+			    				<input type="password" name="password" class="form-control">
+			    				@if($errors->has('password'))
+			    					<span class="help-block">{{ $errors->first('password') }}</span>
+			    				@endif
+			    				@if(isset($wrongPassword))
+			    					<span class="help-block">{{ $wrongPassword }}</span>
+			    				@endif
+			    			</div>
+			    			<button type="submit" class="btn btn-primary btn-block">Login</button>
+			    		</form>
+			    	</div>
+			    </div>
+    		</div>
+    	</div> -->
     </div>
-
-    </div>
-</div>
 </div>
 <style type="text/css">
-   /* CSS used here will be applied after bootstrap.css */
-
-
-
-.panel-default {
- opacity: 0.9;
- margin-top:30px;
-}
-.form-group.last {
- margin-bottom:0px;
-
+    .changepage {
+    padding: 12px 0px;
+    height: 50%;
+    width: 30%;
+    position: absolute;
+    background-color: whitesmoke;
+    margin-top: 100px;
+    margin-left: 25%;
+    border-radius: 20px;
+    /*opacity: 0.8;*/
 }
 </style>
 @endsection
