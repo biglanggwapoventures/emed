008314b97583acc5cb4e5da4d45fa5a0557d576a
Nikki Gallego <mdag_03@yahoo.com>
2017-04-28 19:19:40 +0800

    legot pharma home

--- resources/views/pharmacists/pharma-home.blade.php
+++ resources/views/pharmacists/pharma-home.blade.php
@@ -1,24 +1,14 @@
 @extends('welcome') @section('body')
 <div class="content-wrapper">
-    @if(session('ACTION_RESULT'))
-    <div class="row">
-
-        <div class="col-md-6 col-md-offset-3">
-           <div class="alert alert-{{ session('ACTION_RESULT')['type'] }} text-center" role="alert">
-                {{ session('ACTION_RESULT')['message'] }}
-            </div>
-        </div>
-    </div>
-    @endif
     <!-- Content Header (Page header) -->
-  <section class="content-header">
+     <section class="content-header">
         <h1>
            User Profile
             <small></small>
         </h1>
         <ol class="breadcrumb">
-            <li><a href="/pharma-home"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li><a href="/pharma-home"><i class="fa fa-user"></i> {{ Auth::user()->fullname() }}</a></li>
+            <li><a href="/pmanager-home"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li><a href="/pmanager-home"><i class="fa fa-user"></i> {{ Auth::user()->fullname() }} </a></li>
             
         </ol>
     </section>
@@ -32,24 +22,27 @@
                 <!-- Profile Image -->
                 <div class="box box-primary">
                     <div class="box-body box-profile">
-                        <center><img alt="User Pic" src="{{ "/storage/{$items->userInfo->avatar}" }}" style="width: 150px; height: 150px;" class="img-circle img-responsive"></center>
 
-                        <h3 class="profile-username text-center"> </h3>
+                         @forelse($items AS $items) 
+                         @if($items->userInfo->username === Auth::user()->username)
+                        <h3 class="profile-username text-center">{{ $items->userInfo->fullname()}} </h3>
 
-                        <p class="text-muted text-center">{{ $items->userInfo->fullname() }}</p>
+                        <p class="text-muted text-center">Manager</p>
 
                         <ul class="list-group list-group-unbordered">
                             <li class="list-group-item">
-                                <i class="fa fa-envelope-o" aria-hidden="true"></i> <b>{{ $items->userInfo->email }}</b>
+                                <i class="fa fa-envelope-o" aria-hidden="true"></i> <b>{{$items->userInfo->email}}</b>
                             </li>
                             <li class="list-group-item">
                                 <i class="fa fa-home" aria-hidden="true"></i><b>{{ $items->userInfo->address }}</b>
-                                <li class="list-group-item">
-                                    <center><a href="{{ route( 'patients.edit', [ 'id'=> $items->id]) }}" class= "btn btn-info"><i class="fa fa-pencil"></i> <span>Edit Profile</span></a></center>
-                
-                                </li>
+                            </li>
+                            <li class="list-group-item">
+                                <i class="fa fa-phone" aria-hidden="true"></i><b>{{ $items->userInfo->contact_number }}</b>
+                            </li>
                         </ul>
-
+                        @endif @empty
+                        <p>There are no users yet!</p>
+                        @endforelse
                     </div>
                     <!-- /.box-body -->
                 </div>
@@ -61,48 +54,60 @@
             <!-- /.col -->
             <div class="col-md-9">
                 <div class="nav-tabs-custom">
-                    <ul class="nav nav-tabs">
-                        <li class="active"><a href="#profile" data-toggle="tab">Profile</a></li>
-                    </ul>
                     <div class="tab-content">
                         <!-- tab start -->
-                        <div class="active tab-pane" id="profile">
+                        <div class="active tab-pane" id="activity">
+                            <a href="{{ route('managers.edit', ['id' => $items->userInfo->id]) }}" class="btn btn-info pull-right" data-toggle="tooltip" id="myTooltip" title="Edit Profile"><span class="glyphicon glyphicon-edit"></a>
                             <table class="table table-user-information">
                                 <tbody>
+
                                     <tr>
-                                        <td><span class="glyphicon glyphicon-user"></span> &nbsp<b>Username</b> <br>{{ $items->userInfo->username }}</td>
-                                        <td><span class="glyphicon glyphicon-envelope"></span> <b>Email</b> <br>{{$items->userInfo->email}}</td>
-                                        <td><span class="glyphicon glyphicon-baby-formula"></span> <b>Date of Birth</b><br>{{ $items->userInfo->birthdate }}</td>
+                                        @if(session('ACTION_RESULT'))
+                                        <div class="row">
+                                            <div class="col-md-6 col-md-offset-3">
+                                               <div class="alert alert-{{ session('ACTION_RESULT')['type'] }} text-center" role="alert">
+                                                    {{ session('ACTION_RESULT')['message'] }}
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                        @endif
+                                    </tr>
+                                    <tr><b><h3>Personal information</h3></tr></b></tr>
+                                    <tr>
+                                        <td><span class="glyphicon glyphicon-user"></span> &nbsp<b>Username:</b> <br> {{ $items->userInfo->username }}</td>
+                                        <td><span class="glyphicon glyphicon-envelope"></span> <b>Email:</b> <br> {{$items->userInfo->email}}</td>
                                     </tr>
 
                                     <tr>
-                                        <td><span class="glyphicon glyphicon-tint"></span> <b>Bloodtype</b> <br>{{ $items->bloodtype }}</td>
-                                        <td><i class="fa fa-venus-mars" aria-hidden="true"></i> &nbsp <b>Gender</b> <br>{{ $items->userInfo->sex }}</td>
-                                        <td><span class="glyphicon glyphicon-phone"></span> <b>Phone Number</b> <br>{{ $items->userInfo->contact_number }}</td>
+                                        <td><span class="glyphicon glyphicon-baby-formula"></span> <b>Date of Birth:</b><br> {{ $items->userInfo->birthdate }}</td>
+                                        <td><i class="fa fa-venus-mars" aria-hidden="true"></i> &nbsp <b>Gender:</b> <br>{{ $items->userInfo->sex }}</td>
                                     </tr>
                                     <tr>
-                                        <td><i class="fa fa-gavel" aria-hidden="true"></i><b>Civil Status</b> <br>{{ $items->civilstatus }}</td>
-                                        <td><span class="glyphicon glyphicon-briefcase"></span> <b>Occuptation</b><br>{{ $items->occupation }}</td>
-                                        <td><span class="glyphicon glyphicon-flag"></span> <b>Nationality</b><br>{{ $items->nationality }}</td>
+                                        <td><span class="glyphicon glyphicon-phone"></span> <b>Drugstore:</b> <br> {{ $items->drugstore}}</td>
+                                        <td><i class="fa fa-user-md" aria-hidden="true"></i></span> <b>Drugstore:</b> <br> {{ $items->drugstore_branch }}</td>
                                     </tr>
+
+
                                 </tbody>
                             </table>
+
                         </div>
+
+
+
+
+                        <!-- /.tab-content -->
                     </div>
-                    <!-- /.tab-content -->
+                    <!-- /.nav-tabs-custom -->
                 </div>
-                <!-- /.nav-tabs-custom -->
+                <!-- /.col -->
             </div>
-            <!-- /.col -->
-        </div>
-        <!-- /.row -->
+            <!-- /.row -->
 
     </section>
     <!-- /.content -->
-</div>
-
-
-
+    </div>
 <script type="text/javascript">
  window.setTimeout(function() {
     $(".alert").fadeTo(500, 0).slideUp(500, function(){
@@ -110,4 +115,8 @@
     });
 }, 1000);
 </script>
-@endsection
+
+
+
+
+    @endsection
