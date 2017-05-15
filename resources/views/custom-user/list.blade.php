cb88a2d62ea6c4fab6575c911d1b519687a95f4e
Nikki Gallego <mdag_03@yahoo.com>
2017-05-15 23:05:43 +0800

    div tarong

--- resources/views/custom-user/list.blade.php
+++ resources/views/custom-user/list.blade.php
@@ -21,17 +21,17 @@
         </section>
 
         <section class="content">
-            <div class="row">
-                 @if(session('ACTION_RESULT'))
-    <div class="row">
+            <!-- <div class="row"> -->
+                                                     @if(session('ACTION_RESULT'))
+                                        <div class="row">
 
-        <div class="col-md-6 col-md-offset-3">
-            <div class="alert alert-{{ session('ACTION_RESULT')['type'] }} text-center">
-                {{ session('ACTION_RESULT')['message'] }}
-            </div>
-        </div>
-    </div>
-    @endif
+                                            <div class="col-md-6 col-md-offset-3">
+                                                <div class="alert alert-{{ session('ACTION_RESULT')['type'] }} text-center">
+                                                    {{ session('ACTION_RESULT')['message'] }}
+                                                </div>
+                                            </div>
+                                        </div>
+                                        @endif
                 <div class="col-xs-12">
                     @if(EMedHelper::hasTargetActionPermission($role->name, 'ADD'))
                         <?php
@@ -54,7 +54,7 @@
                     @endif
                 </div>
                 <div class="hidden-xl hidden-lg hidden-md hidden-sm">&nbsp;</div>
-            </div>
+            <!-- </div> -->
             <div class="row">
                 <div class="col-xs-12">
                     <div class="box-body table-responsive no-padding">
@@ -205,14 +205,14 @@
 
                     </div>
 
-                    <div class="row" style="margin-top:10px;">
-                        <div class="col-md-12">
-                            <button style="width:140px;margin-left:5px;" type="button" class="btn btn-primary grey pull-right" data-dismiss="modal">
-                                Close 
-                            </button>
-                        </div>
-                            
-                    </div>
+                            <div class="row" style="margin-top:10px;">
+                                <div class="col-md-12">
+                                    <button style="width:140px;margin-left:5px;" type="button" class="btn btn-primary grey pull-right" data-dismiss="modal">
+                                        Close 
+                                    </button>
+                                </div>
+                                    
+                            </div>
                 </div>
             </div>
         </div>
