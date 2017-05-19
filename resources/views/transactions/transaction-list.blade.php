350be5914dcd2d52ac6e36f5907ed80bb1f325c3
Ariel Badilles <acbadz@gmail.com>
2017-05-19 16:02:48 +0800

    Transaction updates

--- resources/views/transactions/transaction-list.blade.php
+++ resources/views/transactions/transaction-list.blade.php
@@ -7,13 +7,13 @@
                     <li class="breadcrumb-item">
                         <a href="{{ url(session('homepage') . '') }}">Home</a>
                     </li>
-                    <li class="breadcrumb-item active">Prescription Transactions List</li>
+                    <li class="breadcrumb-item active">Transaction History List</li>
                 </ol>
             </div>
             <h1>
                 <span style="font-size:80% !important;">
                     <span class="fa fa-hospital-o" style="font-size:135%!important"></span>
-                    &nbsp;List of Prescription Transactions
+                    &nbsp;List of Transaction History
                 </span>
             </h1>
         </section>
@@ -21,17 +21,15 @@
         <section class="content">
             <div class="row">
                 <div class="col-xs-12">
-                    {{-- var_dump($userdata) --}}
                     <h4>{{ $userdata->pharmacy . ", " . $userdata->pharmacyBranch }}</h4>
                     <h5>{{ $userdata->pharmacyBranchAddress }}</h5>
                 </div>
-                
             </div>
 
             <div class="row">
                 <div class="col-xs-12">
                     <div class="box-body table-responsive no-padding"><br>
-                        <table class="table table-bordered table-striped"">
+                        <table id="example1" class="table table-bordered table-striped"">
                             <thead>
                                 <tr class="active" style="height: 50px">
                                     <th class="align-th">Date of Purchase</th>
@@ -47,28 +45,18 @@
                             <tbody id="userdata">
                                 @forelse($items AS $item)
                                     <tr>
-                                        <?php 
-                                            $rowCount = $item['doctorCount']; 
-                                            $doctors = $item['doctors'];
-                                            $patientId = $item['patient_id'];
-                                        ?>
-                                        <td class="align-pt" rowspan="{{ $rowCount }}">
-                                            {{ $item['firstname'] . " " . $item['lastname'] }}
+                                        <td class="align-pt">{{ $item->purchaseTimestamp }}</td>
+                                        <td class="align-pt">{{ $item->patientName }}</td>
+                                        <td class="align-pt">Dr. {{ $item->doctorName }}</td>
+                                        <td class="align-pt">{{ $item->brand }}</td>
+                                        <td class="align-pt">{{ $item->genericname }}</td>
+                                        <td class="align-pt">{{ $item->purchaseQty }}</td>
+                                        <td class="align-pt">{{ $item->pharmaName }}</td>
+                                        <td class="text-center">
+                                            <a href="#" class="btn btn-default" style="background-color:#999;color:#FFF;margin-left:15px">
+                                                <span class="fa fa-sitemap action-icon"></span>
+                                            </a>
                                         </td>
-                                        
-                                        @foreach($doctors AS $doctor)
-                                            <td class="align-pt">Dr. {{ $doctor['firstname'] . " " . $doctor['lastname'] }}</td>
-                                            <td class="align-pt">{{ $doctor['prescriptionCount'] }} active prescription{{ $doctor['prescriptionCount'] > 1  ? 's' : '' }}</td>
-                                            <td class="text-center">
-                                                <a href="{{ url('pharmatransaction', $patientId) }}" class="btn btn-default" style="background-color:#999;color:#FFF;margin-left:15px">
-                                                    <span class="fa fa-sitemap action-icon"></span>
-                                                </a>
-                                            </td>
-
-                                            @if(--$rowCount > 0)
-                                                </tr><tr>
-                                            @endif
-                                        @endforeach
                                     </tr>
                                 @empty
                                     <tr>
