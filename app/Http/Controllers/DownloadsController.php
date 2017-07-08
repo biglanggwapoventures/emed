0e545f54904a2905db0bd6e1b691016b33e27015
Nikki Gallego <mdag_03@yahoo.com>
2017-07-08 23:43:27 +0800

    no comment

--- app/Http/Controllers/DownloadsController.php
+++ app/Http/Controllers/DownloadsController.php
@@ -8,20 +8,7 @@ class DownloadsController extends Controller
 {
     public function getDownload()
     {
-        //PDF file is stored under project/public/download/info.pdf
-  //      $file= public_path(). "\downloads\localhost.jar";
-
-  //   	$headers = [ 'Content-Type' => 'application/java-archive',];
-
-		// return response()->download($file, 'localhost.jar', $headers);
-
-		// return response()->download(public_path('downloads\localhost.jar'));
-
-		// $filename = public_path("downloads/localhost.jar");
-  //       $headers = ['Content-Type: application/java-archive'];
-  //       $newName = 'rfid-'.time().'.jar';
-
-       // return response()->download($myFile, $newName, $headers);
+     
         return response()->download(storage_path("app/public/downloads/localhost.jar"));
 	}
 }
