fb69d4d6658eb962e1252b83b40227f32082a31e
Nikki Gallego <mdag_03@yahoo.com>
2017-07-20 18:08:56 +0800

    light ui

--- resources/views/others/downloads.blade.php
+++ resources/views/others/downloads.blade.php
@@ -7,22 +7,25 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/body.css') }}">    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
     @include('partials.navbar')
 <br><br><br><br><br><br>
-    <div class="row" style="margin-right: 50px; margin-left: 50px">
-         <div class="col-xs-12">
+    <div class="row" style="margin-left: 400px">
+         <div class="col-xs-6">
                 <div class="box-body table-responsive no-padding">
                         <table id="example1" class="table table-bordered table-striped">
 
                                 <thead>
-                                    <th>Download This</th>
-                                    <th>Para sa rfid</th>
+                                    <th>Download</th>
+                                    <th>Specifications</th>
+                                    <th></th>
                                 </thead>
-                                <td ><a href="/downloadjar" class="btn btn-large pull-left"><i class="icon-download-alt"> </i> Download jar file </a></td>
-                                <td>Para sa tanan os windows or mac</td>
+                                <td ><a href="/downloadjar" class="btn btn-large pull-left"><i class="fa fa-download" aria-hidden="true"></i> &nbsp Download jar file  (4.55 MB ) </a></td>
+                                <td>Windows or Mac</td>
+                                <td>Run file to use rfid</td>
                         </table>
                 </div>
         </div>
